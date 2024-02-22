78d5d2a8bd70466cb8e8e1d171eeefd191e6ec79
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-22 19:36:40 +0200

    contact form

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,24 +1,30 @@
 import { Formik, Form, Field } from 'formik';
+import { useId } from 'react';
 
 const initialValues = {
   username: '',
   phonenumber: '',
 };
 
-const handleSubmit = (values, actions) => {
-  console.log(values);
-  actions.resetForm();
-};
-
 export default function ContactForm() {
+  const nameId = useId();
+  const phoneId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
   return (
     <>
       <p>Form</p>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form>
-          <label htmlFor="">Name</label>
-          <Field type="text" name="username" />
-          <Field type="tel" name="phonenumber" />
+          <label htmlFor={nameId}>Name</label>
+          <Field type="text" name="username" id={nameId} />
+
+          <label htmlFor={phoneId}>Number</label>
+          <Field type="tel" name="phonenumber" id={phoneId} />
+
           <button type="submit">Add contact</button>
         </Form>
       </Formik>
