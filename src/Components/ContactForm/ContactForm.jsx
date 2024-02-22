8737f914e42686bf6979ae37de507723522fe072
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-22 21:19:39 +0200

    error

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,10 +1,20 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { useId } from 'react';
+import * as Yup from 'yup';
+import 'yup-phone';
 
 const initialValues = {
   username: '',
   phonenumber: '',
 };
+
+const ContactSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Too short!')
+    .max(20, 'Too long!')
+    .required('Required'),
+  phonenumber: Yup.number().min(8).required('A phone number is required'),
+});
 
 export default function ContactForm() {
   const nameId = useId();
@@ -16,14 +26,23 @@ export default function ContactForm() {
   };
   return (
     <>
-      <p>Form</p>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={ContactSchema}
+      >
         <Form>
-          <label htmlFor={nameId}>Name</label>
-          <Field type="text" name="username" id={nameId} />
+          <div>
+            <label htmlFor={nameId}>Name</label>
+            <Field type="text" name="username" id={nameId} />
+            <ErrorMessage name="username" component="span" />
+          </div>
 
-          <label htmlFor={phoneId}>Number</label>
-          <Field type="tel" name="phonenumber" id={phoneId} />
+          <div>
+            <label htmlFor={phoneId}>Number</label>
+            <Field type="tel" name="phonenumber" id={phoneId} />
+            <ErrorMessage name="phonenumber" component="span" />
+          </div>
 
           <button type="submit">Add contact</button>
         </Form>
