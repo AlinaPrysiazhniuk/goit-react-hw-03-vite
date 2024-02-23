84abeb79f50f77c9c9e0b8161831cc107cc861d8
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-23 17:49:21 +0200

    styles

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/prop-types */
 import css from './Contact.module.css';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { IoMdContact } from 'react-icons/io';
 
 export default function Contact({ data: { id, name, number }, onDelete }) {
   return (
     <>
       <div>
-        <p>{name}</p>
-        <p>{number}</p>
+        <p className={css.item_info}>
+          <IoMdContact className={css.icon_man} />
+          {name}
+        </p>
+        <p className={css.item_info}>
+          <BsFillTelephoneFill className={css.icon_number} />
+          {number}
+        </p>
       </div>
 
       <button
