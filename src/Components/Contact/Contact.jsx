05165506ef2edf77d3c814eb5c55f16754b40ec7
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-23 17:09:45 +0200

    styles

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-// import css from './Contact.module.css'
+import css from './Contact.module.css';
 
 export default function Contact({ data: { id, name, number }, onDelete }) {
   return (
@@ -9,7 +9,11 @@ export default function Contact({ data: { id, name, number }, onDelete }) {
         <p>{number}</p>
       </div>
 
-      <button type="button" onClick={() => onDelete(id)}>
+      <button
+        type="button"
+        onClick={() => onDelete(id)}
+        className={css.btn_delete}
+      >
         Delete
       </button>
     </>
