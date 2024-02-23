614f5dd37ee4db46a495e117af0dc5f13631b275
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-23 12:26:24 +0200

    contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,13 +2,11 @@
 export default function Contact({ data: { id, name, number }, onDelete }) {
   return (
     <div>
-      <li>
-        <p>{name}</p>
-        <p>{number}</p>
-        <button type="button" onClick={() => onDelete(id)}>
-          Delete
-        </button>
-      </li>
+      <p>{name}</p>
+      <p>{number}</p>
+      <button type="button" onClick={() => onDelete(id)}>
+        Delete
+      </button>
     </div>
   );
 }
